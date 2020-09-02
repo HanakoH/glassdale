e25017ec2877f3764fe87ca8dc04f83d40252167
Hanako Hashiguchi <hanako.e.hashiguchi@gmail.com>
2020-09-02 12:53:34 -0500

    added filtering for officers and a bit for criminals too

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,18 +1,30 @@
 import { getCriminals, useCriminals } from "./CriminalProvider.js";
 import { CriminalHTML } from "./Criminal.js"
 
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("crimeChosen", event => {
+    if (event.detail.crimeThatWasChosen !== "0") {
+        const matchingCriminals = useCriminals().filter(currentCriminal => {
+            return currentCriminal.conviction === event.detail.crimeThatWasChosen
+        })
+       render(matchingCriminals)
+    } else render(useCriminals());
+})
+
 export const CriminalList = () => {
     getCriminals()
     .then(()=>{
         const criminalArray = useCriminals();
-        addCriminalsToDOM(criminalArray);
+        render(criminalArray);
     })
 }
 
-const addCriminalsToDOM = (anCriminalArray) => {
+
+const render = criminalCollection => {
     const domElement = document.querySelector(".criminalsContainer");
 
-    let HTMLArray = anCriminalArray.map(singleCriminal => {
+    let HTMLArray = criminalCollection.map(singleCriminal => {
         return CriminalHTML(singleCriminal);
     })
     domElement.innerHTML = HTMLArray.join("");
--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,9 @@ console.log("Keep up the good work Champ! ^-^");
 import { OfficerList } from "./officers/OfficerList.js"
 import { CriminalList } from "./criminals/CriminalList.js"
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js"
+import { OfficerSelect } from "./officers/OfficerSelect.js"
 
 OfficerList()
 CriminalList()
 ConvictionSelect()
+OfficerSelect()
