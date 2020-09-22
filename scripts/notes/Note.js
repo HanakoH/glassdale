b3de6559dd8224df31184438a6672b2443c1f7ff
Hanako Hashiguchi <hanako.e.hashiguchi@gmail.com>
2020-09-21 22:05:37 -0500

    started to refactor

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -2,7 +2,7 @@ export const NoteHTML = (noteObj) => {
     return `
     <section id="note-${noteObj.id}" class="card-note">
          <p>${noteObj.noteText}</p>
-         <p>Criminal: ${noteObj.suspect}</p>
+         <p>Criminal: ${noteObj.suspectObj.name}</p>
          <p>Date: ${new Date(noteObj.noteDate).toLocaleDateString('en-US')}</p>
      </section>
      `
--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,25 +1,32 @@
 import { getNotes, useNotes } from "./NoteProvider.js";
 import { NoteHTML } from "./Note.js"
+import { getCriminals, useCriminals } from "../criminals/CriminalProvider.js";
 
 const eventHub = document.querySelector(".container")
 
 eventHub.addEventListener("noteStateChanged", event => {  
-    render(useNotes())
+    render(useNotes(), useCriminals())
 })
 
-export const NoteList = () => {
-    getNotes()
-    .then(()=>{
-        const noteArray = useNotes();
-        render(noteArray);
-    })
-}
-
-const render = noteCollection => {
+const render = (noteArray, suspects) => {
     const domElement = document.querySelector(".notesContainer");
 
-    let HTMLArray = noteCollection.map(singleNote => {
+    let HTMLArray = noteArray.map(singleNote => {
+        singleNote.suspectObj = suspects.find(suspect => {
+            return suspect.id === parseInt(singleNote.suspectId)
+        })
+        debugger
         return NoteHTML(singleNote);
     })
     domElement.innerHTML = HTMLArray.join("");
+}
+
+export const NoteList = () => {
+    getNotes()
+    .then(getCriminals)
+    .then(() => {
+        const noteArray = useNotes();
+        const suspects = useCriminals();
+        render(noteArray, suspects);
+    })
 }
